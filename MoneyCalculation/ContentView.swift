94d55c41a7c81e1e98274e538b1fc534b382db98
Saikaku <grnmnsta@gmail.com>
2025-03-14 18:07:23 +0900

    Initial commit

---
+++ MoneyCalculation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoneyCalculation
+//
+//  Created by cmStudent on 2025/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoneyCalculation/MoneyCalculationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyCalculationApp.swift
+//  MoneyCalculation
+//
+//  Created by cmStudent on 2025/03/14.
+//
+
+import SwiftUI
+
+@main
+struct MoneyCalculationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
